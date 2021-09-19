ca87b49cba5568471d5d416ee7d9cbd4ccbfa9e0
Shazni Shiraz <shaznishiraz@gmail.com>
2021-09-19 20:38:12 +0530

    Adding a new API Controller

---
+++ Udemy/DatingApp/Backend/API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var Users = _context.Users.ToList();
+
+            return Users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUsers(int id)
+        {
+            var User = _context.Users.Find(id);
+
+            return User;
+        }
+    }
+}
