11e3a1cbd8fc0395f04124ba81a8ed931550987c
Shazni Shiraz <shaznishiraz@gmail.com>
2021-09-19 20:23:40 +0530

    Creating the database using Entity Framework Code first migrations

--- Udemy/DatingApp/Backend/API/Create Migration.bat
+++ Udemy/DatingApp/Backend/API/Create Migration.bat
@@ -1,2 +1,5 @@
-@REM Create database schema based on classes structure
+@REM Create migrations
 dotnet ef migrations add InitialCreate -o Data/Migrations
+
+@REM Create database
+dotnet ef database update
