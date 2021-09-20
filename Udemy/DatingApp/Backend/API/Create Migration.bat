b85416a359e98898277692b5354eb94a75012c1d
Shazni Shiraz <shaznishiraz@gmail.com>
2021-09-20 13:58:41 +0530

    Updating the user entity

--- Udemy/DatingApp/Backend/API/Create Migration.bat
+++ Udemy/DatingApp/Backend/API/Create Migration.bat
@@ -3,3 +3,9 @@ dotnet ef migrations add InitialCreate -o Data/Migrations
 
 @REM Create database
 dotnet ef database update
+
+@REM update user entity
+dotnet ef migrations add UserPasswordAdded
+
+@REM update database
+dotnet ef database update
