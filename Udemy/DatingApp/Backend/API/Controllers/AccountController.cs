23e07b8f7248faf4750bbe9aaa328a89e06a70d9
Shazni Shiraz <shaznishiraz@gmail.com>
2021-09-20 14:36:04 +0530

    Creating an Account Controller with a register endpoint

---
+++ Udemy/DatingApp/Backend/API/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512();
+
+            AppUser user = new AppUser()
+            {
+                Username = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            await _context.Users.AddAsync(user);
+
+            return user;
+        }
+    }
+}
